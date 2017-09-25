2f6b311fcc24a51b3d7cca4773c756bea6491507
Tabebee <tabibone@gmail.com>
2017-09-25 10:17:24 -0400

    initial files and links created

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCx_kPOmJXXvY-vSqIqFUyPelpKpMDLDEg",
+    authDomain: "train-scheduler-assignme-875fb.firebaseapp.com",
+    databaseURL: "https://train-scheduler-assignme-875fb.firebaseio.com",
+    projectId: "train-scheduler-assignme-875fb",
+    storageBucket: "",
+    messagingSenderId: "457471037516"
+};
+firebase.initializeApp(config);
+
