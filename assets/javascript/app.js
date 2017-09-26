cb388ede7510568c8ef084a076768e6c604fe4ce
Tabebee <tabibone@gmail.com>
2017-09-26 01:22:14 -0400

    layout complete. started js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,18 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCx_kPOmJXXvY-vSqIqFUyPelpKpMDLDEg",
-    authDomain: "train-scheduler-assignme-875fb.firebaseapp.com",
-    databaseURL: "https://train-scheduler-assignme-875fb.firebaseio.com",
-    projectId: "train-scheduler-assignme-875fb",
-    storageBucket: "",
-    messagingSenderId: "457471037516"
-};
-firebase.initializeApp(config);
+
+$(document).ready(function () {
+    // console.log("test");
+    
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCx_kPOmJXXvY-vSqIqFUyPelpKpMDLDEg",
+        authDomain: "train-scheduler-assignme-875fb.firebaseapp.com",
+        databaseURL: "https://train-scheduler-assignme-875fb.firebaseio.com",
+        projectId: "train-scheduler-assignme-875fb",
+        storageBucket: "",
+        messagingSenderId: "457471037516"
+    };
+    firebase.initializeApp(config);
+
+
+});
 
