57a14a889e7c785bd713fc4ab53380483d593649
Tabebee <tabibone@gmail.com>
2017-10-10 15:55:40 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 // Global Variables
-var audio1 = new Audio("assets/audio/steamTrain.mp3");
-var audio2 = new Audio("assets/audio/TaDa.mp3");
+var welcome = new Audio("assets/audio/steamTrain.mp3");
+var successT = new Audio("assets/audio/TaDa.mp3");
 var audio3 = new Audio("assets/audio/TrainWhistle.mp3");
+var denied = new Audio("assets/audio/Denied.mp3");
 var name;
 var destination;
 var firstArrival;
@@ -9,8 +10,8 @@ var frequency;
 var database;
 var trainFirebaseData;
 var newFirebaseData;
-var time = moment().format("hh:mm A");
-
+var time;
+var clock;
 $(document).ready(function () {
 
 //******************* Testing ********************
@@ -18,8 +19,16 @@ $(document).ready(function () {
     // console.log(moment("1432", "hmm").format("HH:mm"));
 //************************************************
 
-    // audio1.play();
-    $("#time").text(time);
+    // welcome.play();
+    // Running Clock at the top
+    function runningClock() {
+        time = moment().format("hh:mm:ss A");
+        $("#time").text(time);
+    }
+    //  Call function with setInterval
+    clock = setInterval(runningClock , 1000);
+
+
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyCx_kPOmJXXvY-vSqIqFUyPelpKpMDLDEg",
@@ -33,66 +42,82 @@ $(document).ready(function () {
 
     database = firebase.database();
 
-    database.ref("Train-Scheduler-App").on("value", function(snapshot) {
-        newFirebaseData = snapshot.val();
-        console.log(newFirebaseData.trainName);
-        var fireBaseName = trainFirebaseData.trainName;
-        var fireBaseDest = trainFirebaseData.destination;
-        var fireBaseFreq = trainFirebaseData.frequency;
-        $("#table-info").append("<tr><td>" + fireBaseName +"</td><td>" + fireBaseDest + "</td><td>" + fireBaseFreq + "</td>");
-
-    });
-
     $("#submitButton").on("click", function (event) {
 
         event.preventDefault();
 
-        name = $("#trainNameInput").val();
-        destination = $("#destinationInput").val();
-        firstArrival = $("#firstTrainTimeInput").val();
-        frequency = $("#frequencyInput").val();
-
-    //    Check if value inside is valid
-        if (name === "" || name === null ||
-            destination === "" || destination === null ||
-            firstArrival === "" || firstArrival === null ||
-            frequency === "" || frequency === null || frequency === 0) {
-            alert("Please kindly enter a valid response in ALL fields.... OR ELSE");
-            clear();
-            return false;
-        }
-
-        //    Put in HTMl Dynamically
-        $("#table-info").append("<tr><td>" + name +"</td><td>" + destination + "</td><td>" + frequency + "</td>");
+        //  Grab Input values IF the conditon above is true
+        name = $("#trainNameInput").val().trim();
+        destination = $("#destinationInput").val().trim();
+        firstArrival = $("#firstTrainTimeInput").val().trim();
+        frequency = $("#frequencyInput").val().trim();
 
 
-//**********************************************************************************************************************    
-    //    Military Time Check       00:00
-    //
-    //     if( isNaN(parseInt(firstArrival.substring(0,2))) || isNaN(parseInt(firstArrival.substring(3)))) {
-    //         alert("Numbers only please");
+//******************** Military Time Not Working************************************************************************
+    // //    Check if value inside is valid. If Valid continue otherwise STOP
+    //     if (name === "" || name === null ||
+    //         destination === "" || destination === null ||
+    //         firstArrival === "" || firstArrival === null ||
+    //         frequency === "" || frequency === null || frequency === 0) {
+    //         alert("Please kindly enter a valid response in ALL fields.... OR ELSE" , denied.play());
     //         return false;
     //     }
     //
-    //     if (firstArrival.length !== 5 || firstArrival.substring(2,3) !== ":") {
-    //         alert("Dang it I said use MILITARY TIME!!!");
+    // //    Check if time is in military time and that they are numbers
+    //     if (firstArrival.length != 5 || parseInt(firstArrival.substr(0,2)) > 23 || parseInt(firstArrival.substr(0,2)) < 0 ||
+    //         parseInt(firstArrival.substr(3,2)) > 59 || parseInt(firstArrival.substr(3,2)) < 0) {
+    //         alert("Check Yo Time Format Fool!" , denied.play());
+    //         return false;
+    //     } else if (isNaN(parseInt(firstArrival.substr(0,2))) || isNaN(parseInt(firstArrival.substr(3,2)))) {
+    //         alert("Only Numbers Fool!" , denied.play());
     //         return false;
     //     }
     //
-    //     if (parseInt(firstArrival.substring(0,2)) < 0 || parseInt(firstArrival.substring(0,2)) > 23 ||
-    //         parseInt(firstArrival.substring(3)) < 0 || parseInt(firstArrival.substring(3)) > 59) {
-    //         alert("Dang it I said use MILITARY TIME!!!");
-    //         return false;
-    //     }
-    //    
+        console.log(firstArrival);
 //**********************************************************************************************************************
 
+
+
+        //  Link and assign variable for firebase
+        trainFirebaseData = {
+            DatatrainName: name,
+            Datadest: destination,
+            DatafirstArrival: firstArrival,
+            Datafrequency: frequency,
+            TimeStamp: firebase.database.ServerValue.TIMESTAMP
+        };
+
+        //    Variable for firebase to link train easier
+        database.ref().push(trainFirebaseData);
 
     //  Make sure fields are back to blank after adding a train
         clear();
 
     });
 
+    database.ref().on("child_added", function (childSnapshot) {
+        //  make variable to ease reference
+        var snapName = childSnapshot.val().DatatrainName;
+        var snapDest = childSnapshot.val().Datadest;
+        var snapFreq = childSnapshot.val().Datafrequency;
+        var snapArrival = childSnapshot.val().DatafirstArrival;
+
+        //  Current Time
+        var timeIs = moment();
+        //  Convert Time and configure for Future use by pushing firstArrival back 1 year
+        var firstArrivalConverted = moment(snapArrival , "HH:mm A").subtract(1, "years");
+        //  Calculate now vs First Arrival
+        var diff = moment().diff(moment(firstArrivalConverted) , "minutes");
+        var left = diff % snapFreq;
+        //  How long till train
+        var timeLeft = snapFreq - left;
+        var newArrival = moment().add(timeLeft , "m").format("HH:mm: A");
+
+        $("#table-info").append("<tr><td>" + snapName +"</td><td>" + snapDest + "</td><td>" + snapFreq + "</td><td>" +
+                                    newArrival + "</td><td>" + timeLeft + "</td></tr>");
+
+
+    });
 
     function clear() {
         $("#trainNameInput").val("");
@@ -122,15 +147,16 @@ $(document).ready(function () {
         // row.append(frequency);
 //**********************************************************************************************************************
 
-//    Variable for firebase to link train easier
-        trainFirebaseData = {
-            trainName: name,
-            destination: destination,
-            firstArrival: firstArrival,
-            frequency: frequency
-        };
-
-        database.ref("Train-Scheduler-App").push(trainFirebaseData);
-
 });
 
+//****************Link Pull From Firebase*******************************************************************************
+// database.ref("Train-Scheduler-App").on("value", function(snapshot) {
+//     newFirebaseData = snapshot.val();
+//     console.log(newFirebaseData.trainName);
+//     var fireBaseName = trainFirebaseData.trainName;
+//     var fireBaseDest = trainFirebaseData.destination;
+//     var fireBaseFreq = trainFirebaseData.frequency;
+//     $("#table-info").append("<tr><td>" + fireBaseName +"</td><td>" + fireBaseDest + "</td><td>" + fireBaseFreq + "</td>");
+//
+// });
+//**********************************************************************************************************************
